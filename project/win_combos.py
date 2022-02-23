947f011e332be083f1ab34c7ba361126be2cfbe1
Nikhil Kankarla <nikhil.kankarla@gmail.com>
2022-02-24 02:40:02 +1100

    Fixed bug in win_combos.py and corresponding test cases

--- project/win_combos.py
+++ project/win_combos.py
@@ -7,22 +7,24 @@ def get_win_combos(rows = 3, columns = 3, num_consecutive_for_win = 3):
     DOWN = 'down'
     DOWN_LEFT = 'down_left'
     DOWN_RIGHT = 'down_right'
-
+    
     TOP_ROW_INDX = 'top_row_indx'
     BOTTOM_ROW_INDX = 'bottom_row_indx'
     LEFT_COLUMN_INDX = 'left_column_indx'
     RIGHT_COLUMN_INDX = 'right_column_indx'
-
+    
+    
     def is_in_outer_perimeter(outer_perimeter_indices, row, column):
         return (
             (row in [outer_perimeter_indices[TOP_ROW_INDX], outer_perimeter_indices[BOTTOM_ROW_INDX]]) or 
             (column in [outer_perimeter_indices[LEFT_COLUMN_INDX], outer_perimeter_indices[RIGHT_COLUMN_INDX]])
         )
     
+    
     def get_directions_to_check_for_position(position: Position, rows = rows, columns = columns, num_consecutive_for_win = num_consecutive_for_win):
         position_row = position.row
         position_column = position.column
-
+        
         directions_to_check_for_position = []
         # Add directions based on position
         # - for positions that are num_consecutive_for_win away from right of board, you can check to the right
@@ -37,8 +39,9 @@ def get_win_combos(rows = 3, columns = 3, num_consecutive_for_win = 3):
             directions_to_check_for_position.append(DOWN_RIGHT)
         if (DOWN in directions_to_check_for_position) and ((position_column + 1) >= num_consecutive_for_win):
             directions_to_check_for_position.append(DOWN_LEFT)
-
+        
         return directions_to_check_for_position
+    
     
     def get_win_combo(position: Position, direction, num_consecutive_for_win = num_consecutive_for_win):
         
@@ -54,7 +57,7 @@ def get_win_combos(rows = 3, columns = 3, num_consecutive_for_win = 3):
                 win_combo.append(Position(position.row + n, position.column - n))
         
         return win_combo
-
+    
     win_combos = []
     outer_perimeter_indices = {
         TOP_ROW_INDX: 0
@@ -62,35 +65,35 @@ def get_win_combos(rows = 3, columns = 3, num_consecutive_for_win = 3):
         , LEFT_COLUMN_INDX: 0
         , RIGHT_COLUMN_INDX: columns - 1
     }
-
+    
     while (
-        ((outer_perimeter_indices[BOTTOM_ROW_INDX] - outer_perimeter_indices[TOP_ROW_INDX]) > 1) and
-        ((outer_perimeter_indices[RIGHT_COLUMN_INDX] - outer_perimeter_indices[LEFT_COLUMN_INDX]))
+        ((outer_perimeter_indices[BOTTOM_ROW_INDX] - outer_perimeter_indices[TOP_ROW_INDX]) >= 1) or
+        ((outer_perimeter_indices[RIGHT_COLUMN_INDX] - outer_perimeter_indices[LEFT_COLUMN_INDX]) >= 1)
     ):
-
+        
         outer_perimeter_positions = []
         for row in range(outer_perimeter_indices[TOP_ROW_INDX], outer_perimeter_indices[BOTTOM_ROW_INDX] + 1):
             for column in range(outer_perimeter_indices[LEFT_COLUMN_INDX], outer_perimeter_indices[RIGHT_COLUMN_INDX] + 1):
                 if is_in_outer_perimeter(outer_perimeter_indices, row, column):
                     outer_perimeter_positions.append(Position(row, column))
-
+        
         for position in outer_perimeter_positions:
             directions_to_check_for_position = get_directions_to_check_for_position(position, rows, columns)
             for direction in directions_to_check_for_position:
                 win_combo = get_win_combo(position, direction, num_consecutive_for_win) # list of tuples
                 win_combos.append(win_combo)
-
+        
         # change outer perimeter to largest inner loop
         outer_perimeter_indices[TOP_ROW_INDX] += 1
         outer_perimeter_indices[BOTTOM_ROW_INDX] -= 1
         outer_perimeter_indices[LEFT_COLUMN_INDX] += 1
         outer_perimeter_indices[RIGHT_COLUMN_INDX] -= 1
-
+    
     win_combos_declassed = []
     # Convert from list of list of Positions to list of list of tuples
     for win_combo in win_combos:
         win_combos_declassed.append([(win_position.row, win_position.column) for win_position in win_combo])
-
+    
     return win_combos_declassed
 
 
@@ -153,21 +156,54 @@ general_board_size_and_num_consecutive_test_cases = [
         , 5
         , 4
         , [
-            [(0, 0), (0, 1), (0, 2), (0, 3)]
-            , [(0, 0), (1, 0), (2, 0), (3, 0)]
-            , [(0, 0), (1, 1), (2, 2), (3, 3)]
-            , [(0, 1), (0, 2), (0, 3), (0, 4)]
-            , [(0, 1), (1, 1), (2, 1), (3, 1)]
-            , [(0, 1), (1, 2), (2, 3), (3, 4)]
-            , [(0, 2), (1, 2), (2, 2), (3, 2)]
-            , [(0, 3), (1, 3), (2, 3), (3, 3)]
-            , [(0, 3), (1, 2), (2, 1), (3, 0)]
-            , [(0, 4), (1, 4), (2, 4), (3, 4)]
-            , [(0, 4), (1, 3), (2, 2), (3, 1)]
-            , [(1, 0), (1, 1), (1, 2), (1, 3)]
-            , [(2, 0), (2, 1), (2, 2), (2, 3)]
-            , [(3, 0), (3, 1), (3, 2), (3, 3)]
-            , [(3, 1), (3, 2), (3, 3), (3, 4)]
+            [(0,0), (0,1), (0,2), (0,3)]
+            , [(0,0), (1,0), (2,0), (3,0)]
+            , [(0,0), (1,1), (2,2), (3,3)]
+            , [(0,1), (0,2), (0,3), (0,4)]
+            , [(0,1), (1,1), (2,1), (3,1)]
+            , [(0,1), (1,2), (2,3), (3,4)]
+            , [(0,2), (1,2), (2,2), (3,2)]
+            , [(0,3), (1,3), (2,3), (3,3)]
+            , [(0,3), (1,2), (2,1), (3,0)]
+            , [(0,4), (1,4), (2,4), (3,4)]
+            , [(0,4), (1,3), (2,2), (3,1)]
+            , [(1,0), (1,1), (1,2), (1,3)]
+            , [(2,0), (2,1), (2,2), (2,3)]
+            , [(3,0), (3,1), (3,2), (3,3)]
+            , [(3,1), (3,2), (3,3), (3,4)]
+            , [(1,1), (1,2), (1,3), (1,4)]
+            , [(2,1), (2,2), (2,3), (2,4)]
+        ]
+    )
+    , pytest.param(
+        4
+        , 4
+        , 3
+        , [
+            [(0, 0), (0, 1), (0, 2)]
+            , [(0, 0), (1, 0), (2, 0)]
+            , [(0, 0), (1, 1), (2, 2)]
+            , [(0, 1), (0, 2), (0, 3)]
+            , [(0, 1), (1, 1), (2, 1)]
+            , [(0, 1), (1, 2), (2, 3)]
+            , [(0, 2), (1, 2), (2, 2)]
+            , [(0, 2), (1, 1), (2, 0)]
+            , [(0, 3), (1, 3), (2, 3)]
+            , [(0, 3), (1, 2), (2, 1)]
+            , [(1, 0), (1, 1), (1, 2)]
+            , [(1, 0), (2, 0), (3, 0)]
+            , [(1, 0), (2, 1), (3, 2)]
+            , [(1, 3), (2, 3), (3, 3)]
+            , [(1, 3), (2, 2), (3, 1)]
+            , [(2, 0), (2, 1), (2, 2)]
+            , [(3, 0), (3, 1), (3, 2)]
+            , [(3, 1), (3, 2), (3, 3)]
+            , [(1, 1), (1, 2), (1, 3)]
+            , [(1, 1), (2, 1), (3, 1)]
+            , [(1, 1), (2, 2), (3, 3)]
+            , [(1, 2), (2, 2), (3, 2)]
+            , [(1, 2), (2, 1), (3, 0)]
+            , [(2, 1), (2, 2), (2, 3)]
         ]
     )
 ]
